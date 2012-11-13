3b94cef8a25f954a093d4875d03a17cc6bbd304e
Caleb Cushing <xenoterracide@gmail.com>
2012-11-12 22:30:15 -0600

    initial implementation of Plugin Config::Merge
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Mojolicious/Plugin/Config/Merge.pm
+++ lib/Mojolicious/Plugin/Config/Merge.pm
@@ -1,16 +1,64 @@
 package Mojolicious::Plugin::Config::Merge;
 use strict;
 use warnings;
-use namespace::autoclean;
+
+## no critic ( RegularExpressions::RequireExtendedFormatting )
+
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Util 'decamelize';
+
+use Module::Load;
+use File::Spec::Functions 'file_name_is_absolute';
+
+use Try::Tiny;
 
 # VERSION
 
-use Moose;
+sub load_config {
+	my ( $self, $path, $conf, $app ) = @_;
 
-__PACKAGE__->meta->make_immutable;
+	$app->log->debug(qq{Reading config files in "$path".});
+
+	my $mode = $app->mode;
+	load 'Config::Merge';
+
+	return Config::Merge->new(
+		path     => $path,
+		is_local => [ qr/^.*\.$mode/ ],
+		skip     => [ qr/^(?:lib|bin|share|t|)$/, qr/^META/, qr/Makefile|Build/i ],
+	);
+}
+
+sub register {
+	my ($self, $app, $conf) = @_;
+
+	# Config path
+	my $path
+		= defined $conf->{path}     ? $conf->{path}
+		: defined $ENV{MOJO_CONFIG} ? $ENV{MOJO_CONFIG}
+		: defined $ENV{MOJO_APP}    ? decamelize( $ENV{MOJO_APP} )
+		:                             basename(   $ENV{MOJO_EXE} )
+		;
+
+	# Absolute paths
+	my $abs_path
+		= file_name_is_absolute $path ? $path
+		:          $app->home->rel_dir( $path )
+		;
+
+	my $config
+		= try {
+			$self->load_config( $abs_path, $conf, $app)
+		}
+		catch {
+			load 'Carp';
+			croak $_;
+		};
+}
+
 1;
 
-# ABSTRACT:
+# ABSTRACT: Config::Merge configuration plugin for Mojolicious
 
 =head1 SYNOPSIS
 
